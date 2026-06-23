42a6ab54a37518959dababa0a72b92fabe684ae7
hopp1t <egoregor3310@gmail.com>
2026-06-23 10:56:23 +0600

    feat: add IsPalindrome extension method with tests

--- practice2025/task01/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace task01
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ practice2025/task01/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return false;
+        }
+
+        var cleanedChars = new List<char>();
+        foreach (char c in input.ToLower())
+        {
+            if (!char.IsWhiteSpace(c) && !char.IsPunctuation(c))
+            {
+                cleanedChars.Add(c);
+            }
+        }
+
+        string cleaned = new string(cleanedChars.ToArray());
+        char[] reversedChars = cleaned.ToCharArray();
+        Array.Reverse(reversedChars);
+        string reversed = new string(reversedChars);
+
+        return cleaned == reversed;
+    }
+}

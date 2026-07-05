6c38e095f8dad17a154d5574b5cb7e129a8cd044
hopp1t <egoregor3310@gmail.com>
2026-07-05 19:14:01 +0600

    feat: implement Add method using Reflection.Emit (not yet GREEN)

--- practice2025/task11/CalculatorGenerator.cs
+++ practice2025/task11/CalculatorGenerator.cs
@@ -1,9 +1,43 @@
+using System.Reflection;
+using System.Reflection.Emit;
+
 namespace task11;
 
 public static class CalculatorGenerator
 {
     public static ICalculator CreateCalculator()
     {
-        throw new NotImplementedException("Need to use Reflection.Emit");
+        var assemblyName = new AssemblyName("DynamicCalculatorAssembly");
+        var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(
+            assemblyName, AssemblyBuilderAccess.Run);
+
+        var moduleBuilder = assemblyBuilder.DefineDynamicModule("DynamicCalculatorModule");
+
+        var typeBuilder = moduleBuilder.DefineType(
+            "DynamicCalculator",
+            TypeAttributes.Public | TypeAttributes.Class,
+            null,
+            new[] { typeof(ICalculator) });
+
+        GenerateMethod(typeBuilder, nameof(ICalculator.Add), OpCodes.Add);
+
+        var calculatorType = typeBuilder.CreateType();
+        return (ICalculator)Activator.CreateInstance(calculatorType)!;
+    }
+
+    private static void GenerateMethod(TypeBuilder typeBuilder, string methodName, OpCode operation)
+    {
+        var methodBuilder = typeBuilder.DefineMethod(
+            methodName,
+            MethodAttributes.Public | MethodAttributes.Virtual,
+            CallingConventions.Standard,
+            typeof(int),
+            new[] { typeof(int), typeof(int) });
+
+        var il = methodBuilder.GetILGenerator();
+        il.Emit(OpCodes.Ldarg_1);
+        il.Emit(OpCodes.Ldarg_2);
+        il.Emit(operation);
+        il.Emit(OpCodes.Ret);
     }
 }

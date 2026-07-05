a4b1c97a0d3382c70e22d3434a2d89b853f27d11
hopp1t <egoregor3310@gmail.com>
2026-07-05 18:43:22 +0600

    refactor: need for Emit

--- practice2025/task11/CalculatorGenerator.cs
+++ practice2025/task11/CalculatorGenerator.cs
@@ -4,10 +4,6 @@ public static class CalculatorGenerator
 {
     public static ICalculator CreateCalculator()
     {
-        var type = Type.GetType("Calculator");
-        if (type == null)
-            throw new InvalidOperationException("Class Calculator not found");
-        
-        return (ICalculator)Activator.CreateInstance(type)!;
+        throw new NotImplementedException("Need to use Reflection.Emit");
     }
 }

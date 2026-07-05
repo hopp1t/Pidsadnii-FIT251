8296d6432790d303a5f50331b241cdebe391cc51
hopp1t <egoregor3310@gmail.com>
2026-07-05 18:41:54 +0600

    feat: attempt using Activator.CreateInstance (FAIL)

--- practice2025/task11/CalculatorGenerator.cs
+++ practice2025/task11/CalculatorGenerator.cs
@@ -4,6 +4,10 @@ public static class CalculatorGenerator
 {
     public static ICalculator CreateCalculator()
     {
-        throw new NotImplementedException();
+        var type = Type.GetType("Calculator");
+        if (type == null)
+            throw new InvalidOperationException("Class Calculator not found");
+        
+        return (ICalculator)Activator.CreateInstance(type)!;
     }
 }

7fbe82d6624bc33d055702226bb916209609dd2e
hopp1t <egoregor3310@gmail.com>
2026-07-07 11:18:40 +0600

    test: add validation tests (RED)

---
+++ practice2025/task13tests/ValidationTests.cs
@@ -0,0 +1,106 @@
+using task13;
+using Xunit;
+
+namespace task13tests;
+
+public class ValidationTests
+{
+    [Fact]
+    public void Validate_ValidStudent_ShouldNotThrow()
+    {
+        var student = new Student
+        {
+            FirstName = "Иван",
+            LastName = "Петров",
+            BirthDate = new DateTime(2000, 5, 15),
+            Grades = new List<Subject>
+            {
+                new Subject { Name = "Математика", Grade = 5 }
+            }
+        };
+
+        var exception = Record.Exception(() => StudentValidator.Validate(student));
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void Validate_NullFirstName_ShouldThrow()
+    {
+        var student = new Student
+        {
+            FirstName = null,
+            LastName = "Петров",
+            BirthDate = new DateTime(2000, 5, 15),
+            Grades = new List<Subject>()
+        };
+
+        var ex = Assert.Throws<InvalidOperationException>(() => StudentValidator.Validate(student));
+        Assert.Contains("FirstName", ex.Message);
+    }
+
+    [Fact]
+    public void Validate_EmptyLastName_ShouldThrow()
+    {
+        var student = new Student
+        {
+            FirstName = "Иван",
+            LastName = "",
+            BirthDate = new DateTime(2000, 5, 15),
+            Grades = new List<Subject>()
+        };
+
+        var ex = Assert.Throws<InvalidOperationException>(() => StudentValidator.Validate(student));
+        Assert.Contains("LastName", ex.Message);
+    }
+
+    [Fact]
+    public void Validate_FutureBirthDate_ShouldThrow()
+    {
+        var student = new Student
+        {
+            FirstName = "Иван",
+            LastName = "Петров",
+            BirthDate = DateTime.Now.AddYears(1),
+            Grades = new List<Subject>()
+        };
+
+        var ex = Assert.Throws<InvalidOperationException>(() => StudentValidator.Validate(student));
+        Assert.Contains("BirthDate", ex.Message);
+    }
+
+    [Fact]
+    public void Validate_InvalidGrade_ShouldThrow()
+    {
+        var student = new Student
+        {
+            FirstName = "Иван",
+            LastName = "Петров",
+            BirthDate = new DateTime(2000, 5, 15),
+            Grades = new List<Subject>
+            {
+                new Subject { Name = "Математика", Grade = 10 } // оценка вне диапазона
+            }
+        };
+
+        var ex = Assert.Throws<InvalidOperationException>(() => StudentValidator.Validate(student));
+        Assert.Contains("Grade", ex.Message);
+    }
+
+    [Fact]
+    public void Validate_EmptySubjectName_ShouldThrow()
+    {
+        var student = new Student
+        {
+            FirstName = "Иван",
+            LastName = "Петров",
+            BirthDate = new DateTime(2000, 5, 15),
+            Grades = new List<Subject>
+            {
+                new Subject { Name = "", Grade = 5 }
+            }
+        };
+
+        var ex = Assert.Throws<InvalidOperationException>(() => StudentValidator.Validate(student));
+        Assert.Contains("Name", ex.Message);
+    }
+}

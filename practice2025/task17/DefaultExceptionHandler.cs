4c5ab65df912aacbad745e7ece34c822881ab006
hopp1t <egoregor3310@gmail.com>
2026-07-15 00:13:42 +0600

    feat: implement IExceptionHandler and DefaultExceptionHandler

---
+++ practice2025/task17/DefaultExceptionHandler.cs
@@ -0,0 +1,9 @@
+namespace task17;
+
+public class DefaultExceptionHandler : IExceptionHandler
+{
+    public void Handle(ICommand command, Exception exception)
+    {
+
+    }
+}
---
+++ practice2025/task17/IExceptionHandler.cs
@@ -0,0 +1,6 @@
+namespace task17;
+
+public interface IExceptionHandler
+{
+    void Handle(ICommand command, Exception exception);
+}
